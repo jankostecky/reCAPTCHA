8a400a96a0e997b3f1ddacfdf28689200687d063
Jan Kostecký <jan.kostecky@gmail.com>
2022-04-07 11:03:53 +0200

    Fix setting threshold param

--- src/DI/ReCaptchaExtension.php
+++ src/DI/ReCaptchaExtension.php
@@ -31,7 +31,7 @@ final class ReCaptchaExtension extends CompilerExtension
 		$builder = $this->getContainerBuilder();
 
 		$builder->addDefinition($this->prefix('provider'))
-			->setFactory(ReCaptchaProvider::class, [$config['siteKey'], $config['secretKey']]);
+			->setFactory(ReCaptchaProvider::class, [$config['siteKey'], $config['secretKey']. $config['threshold']]);
 	}
 
 	/**
